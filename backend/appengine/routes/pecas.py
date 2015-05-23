9caece2fab6d03b1ab47174a0a24cee52f049cdd
Joaohigor <joaohigor_fortunato@hotmail.com>
2015-05-23 14:29:03 -0300

    Angular Funcionando

--- backend/appengine/routes/pecas.py
+++ backend/appengine/routes/pecas.py
@@ -2,20 +2,19 @@
 from __future__ import absolute_import, unicode_literals
 import json
 from config.template_middleware import TemplateResponse
+from gaepermission.decorator import login_not_required
 from tekton import router
 from gaecookie.decorator import no_csrf
 from pecas_app import pecas_facade
 from routes.rests import new, edit, rest
 from tekton.gae.middleware.redirect import RedirectResponse
 
-
 @no_csrf
 def index(_logged_user):
     contexto={ 'rest_new_path': router.to_path(rest.new),
                 'rest_list_path': router.to_path(rest.index),
                 'rest_delete_path': router.to_path(rest.delete),
-                'rest_edit_path': router.to_path(rest.edit),
-
+                'rest_edit_path': router.to_path(rest.edit)
                }
     return TemplateResponse(contexto)
 
